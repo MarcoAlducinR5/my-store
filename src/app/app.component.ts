73230b9156b3de157ab6632cc59f888e6417e5c9
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2022-12-08 17:46:16 -0600

    Clase de Property Binding para Modificar las propiedades de elementos de HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //Interpolacion de valores
   name = 'Marco';
   age = 18;
   img = "https://www.dzoom.org.es/wp-content/uploads/2012/03/bosque-17-810x540.jpg";
+
+  //Modificar las propiedades de elementos de HTML
+  btnDisabled = true;
+  person = {
+    name: this.name,
+    age: this.age,
+    avatar: this.img
+  }
 }
