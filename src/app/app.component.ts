7ee83b0397cb1acc0dfd2efcbc6e71e601b3f537
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2022-12-08 22:03:48 -0600

    Practicando los eventos parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,4 +34,33 @@ export class AppComponent {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
   }
+  dobleClick:string = "Haz Doble Click";
+  dobleClic(event: Event){
+    const element = event.target as HTMLElement;
+    alert("¡Haz hecho doble click");
+    console.log(element);
+  }
+  texto1:string = "Pasa el mouse aqui";
+  pasaElMouse(event: Event){
+    const element = event.target as HTMLElement;
+    this.texto1 = "¡Bien, pasaste el mouse! ¿Viste que cambio?";
+    element.style.background = "green";
+    // console.log(element.style.background);
+    element.style.color = "RGB(0,0,0)";
+  }
+  alejaElMouse(event: Event){
+    const element = event.target as HTMLElement;
+    this.texto1 = "¡Ups, volvio a cambiar! XD Pasalo otra vez";
+    element.style.background = "RGB(0,12,34)";
+    element.style.color = "white";
+  }
+  mensaje:string = "Selecciona la caja";
+  enfoque(event: Event){
+    const element = event.target as HTMLElement;
+    this.mensaje = "¡Bien, esta enfocado! ¿Viste que cambio?";
+  }
+  desenfoque(event: Event){
+    const element = event.target as HTMLElement;
+    this.mensaje = "¡Chin, ya no tiene enfoque! =( Haz clic de nuevo";
+  }
 }
